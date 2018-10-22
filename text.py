99cfd93359b5c104e01ab354d932307b8aa47904
Joshua Effendy <joshua.effendy@student.rjuhsd.us>
2018-10-22 21:25:45 +0000

    first commit

---
+++ text.py
@@ -0,0 +1,28 @@
+print(
+    """wow
+this 
+really
+works""")
+
+print("today we are messing around").split()
+print("today we are messing around").upper()
+
+print("Trinket")[0]
+print("Trinket")[1]
+print("Trinket")[2]
+print("Trinket")[3]
+print("Trinket")[4]
+print("Trinket")[5]
+print("Trinket")[6]
+
+
+
+print("Trinket")[:0]
+print("Trinket")[:1]
+print("Trinket")[:2]
+print("Trinket")[:3]
+print("Trinket")[:4]
+print("Trinket")[:5]
+print("Trinket")[:6]
+print("Trinket")[:7]
+
---
+++ trinket.py
@@ -0,0 +1,2 @@
+weather = "cloudy"
+print( weather + "wow")
